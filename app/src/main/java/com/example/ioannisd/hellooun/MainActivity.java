8744bc992718e7da80438bfb43262c6879dd2eba
IoanDZaim <ioanzaim@gmail.com>
2015-08-31 13:50:28 +0200

    Put license

--- app/src/main/java/com/example/ioannisd/hellooun/MainActivity.java
+++ app/src/main/java/com/example/ioannisd/hellooun/MainActivity.java
@@ -38,7 +38,7 @@ import java.util.List;
         * You should have received a copy of the GNU Lesser General Public License
         * along with this library.  If not, see <http://www.gnu.org/licenses/>.
         * <p/>
-        * Contributors: Stefaan Ternier
+        * Contributors: Ioannis D. Zaimidis
         * ****************************************************************************
         */
 public class MainActivity extends Activity {
